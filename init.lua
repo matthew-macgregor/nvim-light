ad2eb0ac40397f0fe64a93af22c98eb33e895ad5
Matthew MacGregor <m.macgregor@gmail.com>
2025-10-23 21:44:31 -0500

    Added highlight.

--- init.lua
+++ init.lua
@@ -252,6 +252,20 @@ require('mini.snippets').setup({})
 -- See :help MiniCompletion.config
 require('mini.completion').setup({})
 
+local hipatterns = require('mini.hipatterns')
+hipatterns.setup({
+  highlighters = {
+    -- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+    fixme = { pattern = '%f[%w]()FIXME()%f[%W]', group = 'MiniHipatternsFixme' },
+    hack  = { pattern = '%f[%w]()HACK()%f[%W]',  group = 'MiniHipatternsHack'  },
+    todo  = { pattern = '%f[%w]()TODO()%f[%W]',  group = 'MiniHipatternsTodo'  },
+    note  = { pattern = '%f[%w]()NOTE()%f[%W]',  group = 'MiniHipatternsNote'  },
+
+    -- Highlight hex color strings (`#rrggbb`) using that color
+    hex_color = hipatterns.gen_highlighter.hex_color(),
+  },
+})
+
 -- See :help which-key.nvim-which-key-setup
 require('which-key').setup({
   icons = {
